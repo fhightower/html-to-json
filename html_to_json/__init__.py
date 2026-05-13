0202da7421aa6e12dc22e0684957730f31ba9b19
Floyd Hightower <floyd.hightower27@gmail.com>
2026-05-13 06:26:30 -0400

    Properly bump versions

--- html_to_json/__init__.py
+++ html_to_json/__init__.py
@@ -2,4 +2,4 @@ from .convert_html import convert  # noqa: F401
 from .convert_html_tables import convert_tables  # noqa: F401
 
 __author__ = """Floyd Hightower"""
-__version__ = '2.0.0'
+__version__ = '3.0.0'
